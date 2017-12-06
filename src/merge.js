cd3000e37b25220d6d933276657005859f5b2fdd
erratbi <yerratbi@gmail.com>
2017-12-06 19:07:47 +0000

    merging

--- src/merge.js
+++ src/merge.js
@@ -1,35 +1,46 @@
 import express from 'express';
-import csv from 'fast-csv';
+import parse from 'csv-parse/lib/sync';
+import csv from 'csvtojson';
+import cvss from 'fast-csv';
 import bodyParser from 'body-parser';
 import _ from 'lodash';
 import multer from 'multer';
 import fs from 'fs';
-
-
+import { delimiter } from 'path';
 
 const app = express();
 const upload = multer();
 const PORT = 8080;
 
-app.use(bodyParser.urlencoded( {extended: true} ))
-app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
-app.get("/",  (req, res) => {
-    res.render('index');
+app.get('/', (req, res) => {
+	res.render('index');
 });
 
-app.post("/", upload.array('file', 10),  async (req, res) => {
-    var results = [];
-    await req.files.map(async file => {
-        await csv.fromString(file.buffer.toString(), { delimiter: "\t" }).on("data", data => {
-            const da = _.zipObject(["email", "id", "name", "email", "phone"], data);
-            results.push(da);  
-        }).on('end', async () => {
-            results = _.uniqBy(results, "id");
-        })
-    });
-    console.log(results);
-    res.set({ "Content-Disposition": 'attachment; filename="data.csv"' });
-    return csv.write(results, { headers: true, delimiter: ";", transform: row => ({ Email: row.email, ID: row.id, Name: row.name, Phone: row.phone }) }).pipe(res);
+app.post('/', upload.array('file', 10), async (req, res) => {
+	var results = [];
+	await req.files.map(async file => {
+		return await csv({ delimiter: '\t' })
+			.fromString(file.buffer)
+			.on('end_parsed', obj => results.push(obj));
+	});
+	let main, backup;
+
+	if (Object.keys(results[0][0]).length > Object.keys(results[1][0]).length) {
+		main = results[0];
+		backup = results[1];
+	} else {
+		main = results[1];
+		backup = results[0];
+	}
+
+	main.forEach(row => {
+		const id = row['UID'];
+		const data = _.find(backup, { UID: id });
+		row['Phone'] = data['Phone'];
+	});
+	return res.json(main);
 });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
